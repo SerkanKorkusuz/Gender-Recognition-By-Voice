f2e3c5600c383ae23897043a55419644e98f2fed
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-11-24 22:17:36 +0300

    Update and rename Gender Recognition By Voice (with skicit-learn) to Gender Recognition By Voice (with skicit-learn).py

--- Gender Recognition By Voice (with skicit-learn).py
+++ Gender Recognition By Voice (with skicit-learn).py
@@ -8,3 +8,9 @@ from sklearn.model_selection import train_test_split
 source_url = ("https://raw.githubusercontent.com/SerkanKorkusuz/Gender-Recognition-By-Voice/master/voice.csv")
 
 myData = pd.read_csv(source_url, header = 0)
+
+myData.label = [1 if each == "female" else 0 for each in myData.label]
+
+label = myData.label.values
+att = myData.drop(["label"], axis = 1)
+att = (att - np.min(att)) / (np.max(att) - np.min(att))
